ff592fbd76a7e09f7a8597665ab851be6e5ad1a6
Kojima113 <s1270113@u-aizu.ac.jp>
2022-04-22 17:29:47 +0900

    配列でURLを管理して一致していれば投稿しない

--- index.js
+++ index.js
@@ -12,19 +12,29 @@ const clientTwi = new Twitter({
 
 const tokenDis = ''
 
+const urls = []
+
 clientDis.on('ready', () => {
     console.log(`${clientDis.user.tag}でログインしています。`)
     sendFavs()
 })
 
 const sendFavs = () => {
+    if (urls.length > 1000) {
+        urls.length = 0
+    }
     clientTwi.get('favorites/list', function (error, tweets, response) {
         if (error) throw error;
         for (const i in tweets) {
             try {
                 for (const j in tweets[i].extended_entities.media) {
-                    clientDis.channels.cache.get('チャンネルID').send(tweets[i].extended_entities.media[j].media_url.toString())
-                    console.log(tweets[i].extended_entities.media[j].media_url)
+                    if (urls.includes(tweets[i].extended_entities.media[j].media_url.toString())) {
+                        continue
+                    } else {
+                        urls.push(tweets[i].extended_entities.media[j].media_url.toString())
+                        clientDis.channels.cache.get('チャンネルID').send(tweets[i].extended_entities.media[j].media_url.toString())
+                        console.log(tweets[i].extended_entities.media[j].media_url)
+                    }
                 }
             } catch (e) {
                 console.error(e)
