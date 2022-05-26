84d5c3e8c1e083edd535ef5eb978089e4acdfc54
Kojima113 <s1270113@u-aizu.ac.jp>
2022-05-27 07:14:16 +0900

    a

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const Twitter = require('twitter')
 const { Client, Intents } = require('discord.js')
 const clientDis = new Client({ intents: Object.keys(Intents.FLAGS) })
+const env = process.env
 
 const clientTwi = new Twitter({
-    consumer_key: TWITTER_KEY,
-    consumer_secret: TWITTER_SECRET_KEY,
-    access_token_key: TWITTER_ACCESS_TOKEN,
-    access_token_secret: TWITTER_ACCESS_TOKEN_SECRET
+    consumer_key: env.TWITTER_KEY,
+    consumer_secret: env.TWITTER_SECRET_KEY,
+    access_token_key: env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: env.TWITTER_ACCESS_TOKEN_SECRET
 })
 
 const tokenDis = DISCORD_TOKEN
